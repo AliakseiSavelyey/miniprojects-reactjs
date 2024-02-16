306ecd8fa515039f577a8fd0655a10903867d83c
Алексей Савельев <aliakseisavelyey@gmail.com>
2024-02-16 18:04:55 +0300

    feat: initial project

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import  s from "./counter.module.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const countPlus = () => {
+    setCount(count + 1);
+  };
+
+  const countMinus = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className={s.counter}>
+      <div className="count">{count}</div>
+      <button onClick={countPlus} className={s.countPlus}>
+        Плюс +
+      </button>
+      <button onClick={countMinus} className={s.countMinus}>
+        Минус -
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
